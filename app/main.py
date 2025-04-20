847f5bda6e3fa2ed52ce9bfdf93eeb3da38fe7ea
aldo.delgado3694 <aldo.delgado3694@gmail.com>
2025-04-19 21:28:58 -0600

    Update app for use api pandas

--- app/main.py
+++ app/main.py
@@ -1,6 +1,44 @@
 import read_csv
 import charts
 import utils
+import pandas as pd
+
+def get_population(country_data, heads):
+  population = []
+  for population_year in heads:
+    population.append(int(country_data[0][population_year]))
+  return population
+
+seguir = True
+while seguir:
+  df = pd.read_csv('./app/data.csv')
+  lista_paises = df['Country'].values
+  pais = input("Introduce el pais: " + str(lista_paises) + "\n")
+
+  while pais not in lista_paises:
+    print("El pais no existe \n")
+    pais = input("Introduce el pais: \n")
+  tipo = input("Intrduce el tipo de grafico (default=bar): (bar o pie) \n")
+  #county_data = utils.population_by_country(df, pais)
+  county_data = df[df['Country'] == pais]
+  heads = ['2022 Population','2020 Population','2015 Population','2010 Population','2000 Population','1990 Population','1980 Population','1970 Population']
+  print(county_data)
+  print('***' * 5)
+  print(heads)
+  values = df.loc[df['Country'] == pais, heads].values.flatten().tolist()
+  print(values)
+  heads  = list(map(lambda head: head.split(' ')[0], heads))
+
+  if 'BAR' == tipo.upper():
+    charts.generate_bar_chart(heads, values, pais)
+  else:
+    charts.generate_pie_chart(heads, values, pais)
+  
+  seguir = input("Salir: \n").upper() != "SALIR"
+
+  
+
+'''
 
 def get_population(country_data, heads):
   population = []
@@ -32,7 +70,7 @@ while seguir:
     charts.generate_pie_chart(heads, values, pais)
   
   seguir = input("Salir: \n").upper() != "SALIR"
-
+'''
 
 #RETO 2
 
